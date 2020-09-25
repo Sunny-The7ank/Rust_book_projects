cac49cc81d038fbdac6a25967191c03f0af67919
Sunny-The7ank <firstru7h@gmail.com>
2020-09-25 17:36:33 -0400

    Move notes out of main

--- 01_hello_cargo/src/main.rs
+++ 01_hello_cargo/src/main.rs
@@ -1,26 +1,3 @@
 fn main() {
     println!("Hello, Cargo!");
 }
-
-/*
-####### Hello, Cargo! #######
-Cargo is Rust's build system and package manager. Most rust devs use it to manage projects 
-because it handles a lot of tasks for you, such as building code, downloading libraries, and
-building those libraries.
-(Libraries that your code needs are called dependencies)
-
-The simplest programs like "Hello, world!" don't have any dependencies. More complex programs
-will require the use of cargo. 
-
-####### Creating Projects with Cargo #######
-Basic project creation in cargo uses this syntax:
-    cargo new <project_name>
-This creates a new project named whatever project_name is in a new directory named 
-project_name. 
-Cargo generates 2 files and one directory for us: a Cargo.toml file, and a src directory
-with a main.rs file inside. 
-It also initializes a new Git repo along with a .gitignore. Git files won't be generated
-if you run cargo new inside an existing repo; you can override with: cargo new --vcs=git
-
-
-*/
