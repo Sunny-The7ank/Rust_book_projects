cbaa304a9f7f2c755f200453cc0ac3f237ff501e
Sunny-The7ank <firstru7h@gmail.com>
2020-10-21 20:15:40 -0400

    Make first example loop term after 3

--- 03_06_loops/src/main.rs
+++ 03_06_loops/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
+
+    let mut loop_counter = 0;
+
     loop {
         println!("again!");
+        loop_counter += 1;
+        if loop_counter >= 3 {
+            break;
+        }
     }
+    
 }
